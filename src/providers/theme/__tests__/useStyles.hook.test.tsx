9df50bb3ceff8bae2398b24279c528ebe021ddb6
gladius <gladiusservices@protonmail.com>
2023-11-09 23:14:12 +0100

    feat: create useStyles.hook

---
+++ src/providers/theme/__tests__/useStyles.hook.test.tsx
@@ -0,0 +1,54 @@
+import {it, expect, jest, beforeEach} from '@jest/globals';
+import React, {ComponentType, PropsWithChildren} from 'react';
+import {renderHook} from '@testing-library/react-native';
+import {Mock, UnknownFunction} from 'jest-mock';
+import useStyles from '../useStyles.hook';
+import ThemeProvider from '../theme.provider';
+
+let getStyles: Mock<UnknownFunction>;
+
+beforeEach(() => {
+  getStyles = jest.fn(args => args);
+});
+
+const wrapper: ComponentType<PropsWithChildren> = ({children}) => (
+  <ThemeProvider>{children}</ThemeProvider>
+);
+
+it('should call the callback passed as argument', () => {
+  renderHook(() => useStyles(getStyles), {wrapper});
+
+  expect(getStyles).toBeCalled();
+});
+
+it('should return an object with a palette property', () => {
+  const expectedResult = 'palette';
+
+  const {result} = renderHook(() => useStyles(getStyles), {wrapper});
+
+  expect(result.current).toHaveProperty(expectedResult);
+});
+
+it('should return an object with a spacing property', () => {
+  const expectedResult = 'spacing';
+
+  const {result} = renderHook(() => useStyles(getStyles), {wrapper});
+
+  expect(result.current).toHaveProperty(expectedResult);
+});
+
+it('should return an object with a borderRadius property', () => {
+  const expectedResult = 'borderRadius';
+
+  const {result} = renderHook(() => useStyles(getStyles), {wrapper});
+
+  expect(result.current).toHaveProperty(expectedResult);
+});
+
+it('should return an object with a typography property', () => {
+  const expectedResult = 'typography';
+
+  const {result} = renderHook(() => useStyles(getStyles), {wrapper});
+
+  expect(result.current).toHaveProperty(expectedResult);
+});
---
+++ src/providers/theme/useStyles.hook.ts
@@ -0,0 +1,27 @@
+import {useMemo} from 'react';
+import {ThemeContext, useAppTheme} from './theme.provider';
+
+export type StylesDefaults = Omit<ThemeContext, 'onVariantChange' | 'variant'>;
+
+const useStyles = <T>(getStyles: (defaults: StylesDefaults) => T) => {
+  const theme = useAppTheme();
+
+  return useMemo(
+    () =>
+      getStyles({
+        palette: theme.palette,
+        spacing: theme.spacing,
+        borderRadius: theme.borderRadius,
+        typography: theme.typography,
+      }),
+    [
+      getStyles,
+      theme.borderRadius,
+      theme.palette,
+      theme.spacing,
+      theme.typography,
+    ],
+  );
+};
+
+export default useStyles;
