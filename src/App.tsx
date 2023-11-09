a8657e77b2c72b7217cc0c3d26b6935f0a54f3ac
gladius <gladiusservices@protonmail.com>
2023-11-09 23:26:07 +0100

    fix: safeareaview missing flex

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import ThemeProvider from './providers/theme/theme.provider';
 
 function App(): JSX.Element {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{flex: 1}}>
       <ThemeProvider>
         <Text>StarGazers</Text>
       </ThemeProvider>
