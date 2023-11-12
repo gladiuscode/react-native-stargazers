f2cdbfdc1e3338243230945039dfa399fbf30b90
gladius <gladiusservices@protonmail.com>
2023-11-13 00:47:25 +0100

    feat: implement statusBar handling based on current theme variant

--- src/presentation/providers/theme/theme.provider.tsx
+++ src/presentation/providers/theme/theme.provider.tsx
@@ -1,5 +1,5 @@
 import React, {PropsWithChildren, useCallback, useMemo, useState} from 'react';
-import {useColorScheme} from 'react-native';
+import {Platform, StatusBar, useColorScheme} from 'react-native';
 import contextFactory from '../../../utils/contextFactory/contextFactory.util';
 import palette, {Palette, PaletteVariant} from './palette.theme';
 import getTypographyBy, {Typography} from './typography.theme';
@@ -21,15 +21,27 @@ export {useAppTheme};
 const ThemeProvider: React.FC<PropsWithChildren> = ({children}) => {
   const isDarkMode = useColorScheme() === 'dark';
 
-  const [variant, setVariant] = useState<ThemeContext['variant']>(
-    isDarkMode ? 'light' : 'dark',
-  );
+  const updateStatusBar = useCallback((variant: PaletteVariant) => {
+    StatusBar.setBarStyle(
+      variant === 'dark' ? 'light-content' : 'dark-content',
+    );
+    if (Platform.OS === 'android') {
+      StatusBar.setBackgroundColor(palette[variant].background);
+    }
+  }, []);
+
+  const [variant, setVariant] = useState<ThemeContext['variant']>(() => {
+    const initialVariant = isDarkMode ? 'light' : 'dark';
+    updateStatusBar(initialVariant);
+    return initialVariant;
+  });
 
   const onVariantChange = useCallback<ThemeContext['onVariantChange']>(
     newVariant => {
       setVariant(newVariant);
+      updateStatusBar(newVariant);
     },
-    [],
+    [updateStatusBar],
   );
 
   const value = useMemo<ThemeContext>(
