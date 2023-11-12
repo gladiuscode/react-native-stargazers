d237b1dc449391cf46956fd669734e473d7c123d
gladius <gladiusservices@protonmail.com>
2023-11-12 02:12:43 +0100

    feat: extend client.api get method to handle queryParams

--- src/data/api/client.api.ts
+++ src/data/api/client.api.ts
@@ -1,14 +1,24 @@
-export interface ClientApiCallConfig extends RequestInit {}
+export interface ClientApiCallConfig<Params = {}> extends RequestInit {
+  params?: Params;
+}
 
 export interface IClientApi {
-  get<T>(url: string, config?: ClientApiCallConfig): Promise<T>;
+  get<T, Params extends {}>(
+    url: string,
+    config?: ClientApiCallConfig<Params>,
+  ): Promise<T>;
 }
 
 class ClientApi implements IClientApi {
   constructor(private baseURL: string) {}
 
-  async get<T>(url: string, config?: ClientApiCallConfig): Promise<T> {
-    const response = await fetch(`${this.baseURL}${url}`, config);
+  async get<T, Params = {}>(
+    url: string,
+    config?: ClientApiCallConfig<Params>,
+  ): Promise<T> {
+    const preparedURL = this.prepareURL(url, config?.params);
+
+    const response = await fetch(preparedURL, config);
     if (!response.ok) {
       throw new Error(
         `[CLIENT_API]: ${response.status} | ${response.statusText}`,
@@ -16,6 +26,16 @@ class ClientApi implements IClientApi {
     }
 
     return response.json();
+  }
+
+  private prepareURL<Params = {}>(url: string, params?: Params) {
+    const fullURL = `${this.baseURL}${url}`;
+    if (!params) {
+      return fullURL;
+    }
+
+    const queryParams = new URLSearchParams(params);
+    return `${fullURL}?${queryParams}`;
   }
 }
 
