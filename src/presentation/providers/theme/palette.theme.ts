70c2ba14c6f4ebc0556a5ed2ca31673be9a13113
gladius <gladiusservices@protonmail.com>
2023-11-13 00:47:03 +0100

    feat: update palette dark variant colors

--- src/presentation/providers/theme/palette.theme.ts
+++ src/presentation/providers/theme/palette.theme.ts
@@ -7,17 +7,19 @@ const light = {
   success: '#4ca742',
   successSurface: 'rgb(213,255,213)',
   divider: '#a4a4a4',
+  border: '#191818',
 };
 
 const dark: typeof light = {
-  background: '#ffffff',
-  text: '#191818',
-  textLight: 'rgba(25,24,24,0.37)',
+  background: '#1a1a1a',
+  text: '#f1f1f1',
+  textLight: 'rgba(255,255,255,0.82)',
   error: '#f43838',
   errorSurface: 'rgb(255,185,185)',
   success: '#4ca742',
   successSurface: 'rgb(213,255,213)',
   divider: '#a4a4a4',
+  border: '#f1f1f1',
 };
 
 const palette = {
