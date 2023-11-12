9a407ca8f7a206304e00d4a557e0792f027fe5be
gladius <gladiusservices@protonmail.com>
2023-11-12 20:04:54 +0100

    feat: create useGetRepositoryApi.hook and implement it in useHomepage.facade

--- src/presentation/features/homepage/useHomepage.facade.ts
+++ src/presentation/features/homepage/useHomepage.facade.ts
@@ -1,6 +1,7 @@
 import useStyles from '../../providers/theme/useStyles.hook';
 import getHomepageStyles from './homepage.styles';
 import {useCallback, useState} from 'react';
+import useGetRepositoryApi from '../../hooks/useGetRepositoryApi/useGetRepositoryApi.hook';
 
 interface SearchPayload {
   owner?: string;
@@ -9,10 +10,16 @@ interface SearchPayload {
 
 const useHomepageFacade = () => {
   const styles = useStyles(getHomepageStyles);
-  const [, setSearchPayload] = useState<SearchPayload>({
+  const [searchPayload, setSearchPayload] = useState<SearchPayload>({
     owner: undefined,
     repository: undefined,
   });
+
+  const {
+    data: repositoryEntity,
+    loading,
+    error,
+  } = useGetRepositoryApi(searchPayload);
 
   const onSubmit = useCallback((owner: string, repository: string) => {
     setSearchPayload({
@@ -23,6 +30,9 @@ const useHomepageFacade = () => {
 
   return {
     styles,
+    repositoryEntity,
+    loading,
+    error,
     onSubmit,
   };
 };
---
+++ src/presentation/hooks/useGetRepositoryApi/useGetRepositoryApi.hook.ts
@@ -0,0 +1,42 @@
+import {useCallback, useEffect, useState} from 'react';
+import RepositoryEntity from '../../../domain/entities/repository.entity';
+import {useRepositories} from '../../providers/repositories/repositories.provider';
+
+interface UseGetRepositoryApiParams {
+  owner?: string;
+  repository?: string;
+}
+
+const useGetRepositoryApi = ({
+  owner,
+  repository,
+}: UseGetRepositoryApiParams) => {
+  const repositoryRepository = useRepositories().repository;
+  const [data, setData] = useState<RepositoryEntity>();
+  const [error, setError] = useState<string>();
+
+  const enabled = owner && repository;
+
+  const getRepository = useCallback(async () => {
+    try {
+      setData(await repositoryRepository.getRepository(owner!, repository!));
+    } catch (e) {
+      if (e instanceof Error) {
+        setError(e.message);
+      }
+      setError('Something went wrong');
+    }
+  }, [owner, repository, repositoryRepository]);
+
+  useEffect(() => {
+    if (!enabled) {
+      return;
+    }
+
+    getRepository().then();
+  }, [enabled, getRepository]);
+
+  return {data, loading: !data && !error && enabled, error};
+};
+
+export default useGetRepositoryApi;
